3611ee6c3552740a6a98b9eb5c729ba96b3a4e98
Alex Standiford <a@alexstandiford.com>
2021-05-03 16:26:15 -0700

    Fixes issues where extended plugins did not get registered items.

--- admin-notices.php
+++ admin-notices.php
@@ -4,14 +4,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $class ) {
-	if ( 'Underpin\Underpin' === $class ) {
-		define( 'UNDERPIN_ADMIN_MENU_ROOT_DIR', plugin_dir_path( __FILE__ ) );
-		require_once( UNDERPIN_ADMIN_MENU_ROOT_DIR . 'lib/abstracts/Admin_Notice.php' );
-		require_once( UNDERPIN_ADMIN_MENU_ROOT_DIR . 'lib/factories/Admin_Notice_Instance.php' );
-		Underpin\underpin()->loaders()->add( 'admin_notices', [
-			'instance' => 'Underpin_Admin_Notices\Abstracts\Admin_Notice',
-			'default'  => 'Underpin_Admin_Notices\Factories\Admin_Notice_Instance',
-		] );
-	}
+add_action( 'underpin/before_setup', function ( $instance ) {
+	define( 'UNDERPIN_ADMIN_MENU_ROOT_DIR', plugin_dir_path( __FILE__ ) );
+	require_once( UNDERPIN_ADMIN_MENU_ROOT_DIR . 'lib/abstracts/Admin_Notice.php' );
+	require_once( UNDERPIN_ADMIN_MENU_ROOT_DIR . 'lib/factories/Admin_Notice_Instance.php' );
+	$instance->loaders()->add( 'admin_notices', [
+		'instance' => 'Underpin_Admin_Notices\Abstracts\Admin_Notice',
+		'default'  => 'Underpin_Admin_Notices\Factories\Admin_Notice_Instance',
+	] );
 } );
