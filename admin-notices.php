d9cde08f0c74582fce4c56c8cfa67df52fd84e6b
Alex Standiford <a@alexstandiford.com>
2021-11-21 19:01:50 -0600

    Compat for Underpin 2.0.0

--- admin-notices.php
+++ admin-notices.php
@@ -1,17 +1,22 @@
 <?php
+
+use Underpin\Abstracts\Underpin;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $file, $class ) {
-	if ( ! defined( 'UNDERPIN_ADMIN_NOTICES_ROOT_DIR' ) ) {
-		define( 'UNDERPIN_ADMIN_NOTICES_ROOT_DIR', plugin_dir_path( __FILE__ ) );
-	}
-	require_once( UNDERPIN_ADMIN_NOTICES_ROOT_DIR . 'lib/abstracts/Admin_Notice.php' );
-	require_once( UNDERPIN_ADMIN_NOTICES_ROOT_DIR . 'lib/factories/Admin_Notice_Instance.php' );
-	Underpin\underpin()->get( $file, $class )->loaders()->add( 'admin_notices', [
-		'instance' => 'Underpin_Admin_Notices\Abstracts\Admin_Notice',
-		'default'  => 'Underpin_Admin_Notices\Factories\Admin_Notice_Instance',
-	] );
-}, 10, 2 );
+Underpin::attach( 'setup', new \Underpin\Factories\Observer( 'admin_notices', [
+	'update' => function ( Underpin $plugin ) {
+		if ( ! defined( 'UNDERPIN_ADMIN_NOTICES_ROOT_DIR' ) ) {
+			define( 'UNDERPIN_ADMIN_NOTICES_ROOT_DIR', plugin_dir_path( __FILE__ ) );
+		}
+		require_once( UNDERPIN_ADMIN_NOTICES_ROOT_DIR . 'lib/abstracts/Admin_Notice.php' );
+		require_once( UNDERPIN_ADMIN_NOTICES_ROOT_DIR . 'lib/factories/Admin_Notice_Instance.php' );
+		$plugin->loaders()->add( 'admin_notices', [
+			'instance' => 'Underpin_Admin_Notices\Abstracts\Admin_Notice',
+			'default'  => 'Underpin_Admin_Notices\Factories\Admin_Notice_Instance',
+		] );
+	},
+] ) );
